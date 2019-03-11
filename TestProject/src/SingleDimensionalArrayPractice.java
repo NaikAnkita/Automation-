c1b298877411029e8a8547098b4b4a9a219a6061
Ankita Naik <ankita@systenics.net>
2019-03-11 16:34:10 +0530

    Example Added

--- TestProject/src/SingleDimensionalArrayPractice.java
+++ TestProject/src/SingleDimensionalArrayPractice.java
@@ -3,7 +3,44 @@ public class SingleDimensionalArrayPractice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+			// TODO Auto-generated method stub
+			//Java Program to illustrate how to declare, instantiate, initialize  
+			//and traverse the Java array.  
+			int [] a = new int [6];	//i is length
+			a[0]=10;
+			a[1]=20;
+			a[2]=30;
+			a[3]=40;
+			a[4]=50;
+			a[5]=60;
+			//a[6]=70;
+			//a[7]=80;
+			//a[8]=90;
+			//a[9]=100;
+			
+			for(int i=0; i<a.length; i++)
+			{
+			System.out.println(a[i]);
+		//	System.out.println(a[2]);
+			}
+			
+			//**********************************************OR***************************************************************
+	//
+	//int a[] = {1,2,3,4,5,6};
+	//for (int i=0; i<a.length; i++) {
+//		System.out.println(a[i]);
+	//}	}
 
+			
+//		int b[]={33,3,4,5};//declaring and initializing an array  
+//		min(b);//passing array to method  
+//		
+//		
+//			printArray(new int[]{10,22,44,66});//passing anonymous array to method  
+			
+	}
+	
 	}
 
-}
+
